bcaaa242a7f924b22c361c69b98666b2f6fe2164
drusklo <drusklo5@gmail.com>
2022-04-16 23:00:30 +0200

    Added function to check disks

--- monitor.py
+++ monitor.py
@@ -16,7 +16,11 @@ path = __location__ = os.path.realpath(
 #a = os.system('borg list --last 1 --json > output.json')
 
 # This reads the output.json and compares the date
-f = open(path+'/output.json')
+#f = open(path+'/output.json')
+
+apachedir = '/srv/http/'
+
+f = open('/home/xose/Scripts/backups/output.json')
  
 data = json.load(f)
 
@@ -34,21 +38,21 @@ if date != dateBackup:
     print('This is the current date: '+str(date))
     print('This is the date of the last backup: '+str(dateBackup))
     print('Dates do not match, backup has not run!')
+    output = {
+    "Backup" : "N"
+}
 
 elif date == dateBackup:
     print('This is the current date: '+str(date))
     print('This is the date of the last backup: '+str(dateBackup))
     print('Backup has been completed!')
-
+    output = {
+    "Backup" : "Y"
+}
 
 # This generates a json file
 
-# Data to be written
-output = {
-    "Backup" : "N"
-}
- 
-with open('data.json', 'w') as outfile:
+with open(apachedir+'data.json', 'w') as outfile:
     json.dump(output, outfile, indent=4)
 
 # This consumes the json file from a url and prints the output
@@ -58,4 +62,21 @@ url = 'http://localhost:8080/data.json'
 new_request = requests.get(url)
 json_data = new_request.json()
 
-print(json_data['Backup'])
+#print(json_data['Backup'])
+
+# Test SMART Data
+
+disks = {'sda','sdb','sdc','sdd','sde'}
+
+for disk in disks:
+    print('Checking disk /dev/'+disk)
+    os.system('smartctl --all /dev/'+disk+' -j > /home/xose/sysReports/smartdata.json')
+    report = open('/home/xose/sysReports/smartdata.json')
+    jsonData = json.load(report)
+    jsonOutput = jsonData['ata_smart_data']['self_test']['status']['string']
+    #print(jsonOutput)
+    if jsonOutput == 'completed without error':
+        print('No Errors')
+    else:
+        print('Disk has issue!')
+        pass
