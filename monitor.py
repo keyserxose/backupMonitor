4d49424d7126d9ff616ab8a0d357d01a79a36337
drusklo <drusklo5@gmail.com>
2022-04-30 00:58:35 +0200

    Cleaning up

--- monitor.py
+++ monitor.py
@@ -13,7 +13,7 @@ path = __location__ = os.path.realpath(
 
 #os.environ['BORG_REPO'] = 'Here goes the env variable'
 
-def backupStatus():
+def backupRsync():
     jsonBackup = open('/home/xose/Scripts/backup/output.json')
     data = json.load(jsonBackup)
     format = "%Y-%m-%dT%H:%M:%S.%f"
@@ -34,6 +34,28 @@ def backupStatus():
         print('This is the date of the last backup: '+str(dateBackup))
         print('Backup has been completed!')
         backup = 'Y'
+
+def backupLocal():
+    jsonBackup = open('/home/xose/Scripts/backup/outputLocal.json')
+    data = json.load(jsonBackup)
+    format = "%Y-%m-%dT%H:%M:%S.%f"
+    last_modified = data['repository']['last_modified']
+    backupDate = datetime.strptime(last_modified, format)
+    today = date.today()
+    global dateBackup
+    dateBackup = backupDate.date()
+    global backupLocal
+    if today != dateBackup:
+        print('This is the current date: '+str(today))
+        print('This is the date of the last backup: '+str(dateBackup))
+        print('Dates do not match, backup has not run!')
+        backupLocal = 'N'
+
+    elif today == dateBackup:
+        print('This is the current date: '+str(today))
+        print('This is the date of the last backup: '+str(dateBackup))
+        print('Backup has been completed!')
+        backupLocal = 'Y'
 
 # This consumes the json file from a url and prints the output
 
@@ -59,6 +81,8 @@ def checkDisks():
         except KeyError:
             pass
             
+        global disk0
+        global status0
         global disk1
         global status1
         global disk2
@@ -67,38 +91,36 @@ def checkDisks():
         global status3
         global disk4
         global status4
-        global disk5
-        global status5
         if jsonOutput == 'completed without error' and disk == 'sda':
+            disk0 = disk
+            status0 = 'Good'
+        elif jsonOutput == 'completed without error' and disk == 'sdb':
             disk1 = disk
             status1 = 'Good'
-        elif jsonOutput == 'completed without error' and disk == 'sdb':
+        elif jsonOutput == 'completed without error' and disk == 'sdc':
             disk2 = disk
             status2 = 'Good'
-        elif jsonOutput == 'completed without error' and disk == 'sdc':
+        elif jsonOutput == 'completed without error' and disk == 'sdd':
             disk3 = disk
             status3 = 'Good'
-        elif jsonOutput == 'completed without error' and disk == 'sdd':
+        elif jsonOutput == 'completed without error' and disk == 'sde':
             disk4 = disk
             status4 = 'Good'
-        elif jsonOutput == 'completed without error' and disk == 'sde':
-            disk5 = disk
-            status5 = 'Good'
         elif jsonOutput != 'completed without error' and disk == 'sda':
+            disk0 = disk
+            status0 = 'Issue'
+        elif jsonOutput != 'completed without error' and disk == 'sdb':
             disk1 = disk
             status1 = 'Issue'
-        elif jsonOutput != 'completed without error' and disk == 'sdb':
+        elif jsonOutput != 'completed without error' and disk == 'sdc':
             disk2 = disk
             status2 = 'Issue'
-        elif jsonOutput != 'completed without error' and disk == 'sdc':
+        elif jsonOutput != 'completed without error' and disk == 'sdd':
             disk3 = disk
             status3 = 'Issue'
-        elif jsonOutput != 'completed without error' and disk == 'sdd':
+        elif jsonOutput != 'completed without error' and disk == 'sde':
             disk4 = disk
             status4 = 'Issue'
-        elif jsonOutput != 'completed without error' and disk == 'sde':
-            disk5 = disk
-            status5 = 'Issue'
         else:
             print('There is an issue!')
             pass
@@ -107,18 +129,21 @@ def checkDisks():
 
 def generateJSON():
     apachedir = '/srv/http/'
-    output = [{'backup': backup, 'backups date': str(dateBackup)}, 
-    {'disk1':{'device': disk1, 'status': status1}, 
+    output = [{'backup0':{'backup': backup, 'date': str(dateBackup), 'device':'rsync'},
+    'backup': {'backup1': backupLocal, 'date': str(dateBackup), 'device':'local'}},
+    {'disk0':{'device': disk0, 'status': status0}, 
+    'disk1':{'device': disk1, 'status': status1},
     'disk2':{'device': disk2, 'status': status2},
     'disk3':{'device': disk3, 'status': status3},
-    'disk4':{'device': disk4, 'status': status4},
-    'disk5':{'device': disk5, 'status': status5}},
+    'disk4':{'device': disk4, 'status': status4}},
     ]
     with open(apachedir+'data.json', 'w') as outfile:
         json.dump(output, outfile, indent=4)
 
 
-backupStatus()
+backupRsync()
+
+backupLocal()
 
 checkDisks()
 
